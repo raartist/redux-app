78fdec3df63909fd268b00ec0ebd385c8ebefbf5
raartist <rishabhzee98@gmail.com>
2020-12-28 00:08:19 +0530

    first

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const ADD_TODO = 'ADD_TODO'
+const addTodo = () =>({
+    type:ADD_TODO,
+    message,
+})
+
+const DELETE_TODO = 'DELETE_TODO'
+const deleteTodo = (id) =>({
+    type:DELETE_TODO,
+    message,
+})
